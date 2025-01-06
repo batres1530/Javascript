c173758d1b2fa2035a61a11b8504b25323d00995
David Martinez <tu_correo_de_github@gmail.com>
2025-01-06 00:49:09 -0600

    add funciones

--- Fundamentos/assets/js/objeto-literal.js
+++ Fundamentos/assets/js/objeto-literal.js
@@ -1,4 +1,30 @@
 let personajes = {
-    nombre: 'Fernando',
+    nombre: 'Tony stark',
+    codeName: 'Iron man',
+    vivo:false,
+    edad:40,
+    corords: {
+        lat : 40.416775,
+        long: -3.703790
+    },
+    trajes: ['Mark I','mark V','Hulkbuster'],
+    direccion: {
+        zip: '0800, 90265',
+        ubicacion: 'malibud california'
+        
+    },
+    'ultimaPelicula': 'Avengers: Endgame',
+    
 };
+console.log(personajes);
 console.log('nombre',personajes.nombre);
+console.log('codeName',personajes['nombre']);
+console.log('edad',personajes.edad);
+console.log('lat',personajes.corords.lat);
+personajes.trajes.push('Mark III');
+console.log('Numero de trajes ',personajes.trajes.length);
+console.log('Numero de trajes ',personajes.trajes[personajes.trajes.length-1]);
+
+const x = 'vivo';
+console.log('vivo',personajes[x]);
+console.log('ultimaPelicula',personajes['ultimaPelicula']);
