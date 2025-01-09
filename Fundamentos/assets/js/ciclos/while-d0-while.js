479bdb42824916c78180054959c4c3445de9ceab
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-01-09 17:14:09 -0600

    se añadieron la carpeta de ciclos

--- Fundamentos/assets/js/ciclos/while-d0-while.js
+++ Fundamentos/assets/js/ciclos/while-d0-while.js
@@ -1,1 +1,26 @@
-console.log('pruebas de commit '); 
+const  carro = ['Ford', 'mazda', 'honda', 'toyota'];
+console.warn(' While');
+let i = 0;
+
+// while (i < carro.length) {
+//     console.log(carro[i]);
+//     i++;
+// }
+
+while (carro[i]) {
+    if (i === 1 ) {
+        // break;
+        i++;
+        continue;
+    }
+    console.log(carro[i]);
+    i++;
+}
+
+console.warn('Do While');
+let j=10;
+
+do {
+    Console.log(carro[j]);
+    j++;
+} while(carro[j]);
