25a07a380734eb0412b45040a1e7cd9fa0b10917
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-01-20 12:05:14 -0600

    se implementa la clase Singleton con manejo de instancia única y se añaden ejemplos de uso

--- 01-Fundamentos/assets/js/classes/singleton.js
+++ 01-Fundamentos/assets/js/classes/singleton.js
@@ -1,1 +1,25 @@
-console.log('Hola Mundo');
+// el siglenton es una clase que se instancia en cualquier laddo
+
+class Singleton {
+    
+    static instancia; // el siglenton simepre lleva el static
+    nombre = '';
+
+    constructor(nombre = ''){
+       
+
+        if(!!Singleton.instancia){
+            return Singleton.instancia;
+        }
+        Singleton.instancia = this;
+        this.nombre = nombre;
+    }
+           
+};
+
+const instancia1 = new Singleton('iroman');
+const instancia2 = new Singleton('spiderman');
+const instancia3 = new Singleton('Black Panther');
+console.log (`Nombre de la instancia2 es ${instancia3.nombre}`);
+console.log (`Nombre de la instancia2 es ${instancia2.nombre}`);
+console.log (`Nombre de la instancia1 es ${instancia1.nombre}`);
